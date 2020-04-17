69a1a64271f9be70a31ee30649def38a29956a2b
camilleclara <camilleberrier@hotmail.com>
2020-04-17 20:37:41 +0200

    quizzcontroller is now able to get question and matching answers according to route parameters

--- src/Controller/QuizzController.php
+++ src/Controller/QuizzController.php
@@ -2,18 +2,47 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Reponse;
+use App\Entity\Question;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class QuizzController extends AbstractController
 {
     /**
-     * @Route("/quizz", name="quizz")
+     * @Route("/quizz/{int}", name="quizz")
      */
-    public function index()
+    public function index(Request $req)
     {
+        $num=$req->get("int");
+        $em = $this->getDoctrine()->getManager();
+        $repQ = $em->getRepository(Question::class);
+        $question = $repQ->findOneBy(["id"=>$num]);
+
+        $repR = $em->getRepository(Reponse::class);
+        $reponses = $repR->findBy(['question'=>$num]);
+
         return $this->render('quizz/index.html.twig', [
-            'controller_name' => 'QuizzController',
+            'controller_name' => 'QuizzController', 'question'=> $question, 'reponses'=>$reponses
         ]);
+    }
+    /**
+     * @Route("/quizz/{int}", name="test_quizz")
+     */
+    public function testquizz(Request $req)
+    {
+        $num=$req->get("int");
+        $em = $this->getDoctrine()->getManager();
+        $repQ = $em->getRepository(Question::class);
+        $question = $repQ->findOneBy(["id"=>$num]);
+
+        $repR = $em->getRepository(Reponse::class);
+        $reponses = $repR->findBy(['question'=>$num]);
+
+        return $this->render('quizz/index.html.twig', [
+            'controller_name' => 'QuizzController', 'question'=> $question, 'reponses'=>$reponses
+        ]);
+
     }
 }
